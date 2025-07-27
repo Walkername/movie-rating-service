06315a8e218c88d39c4e5033a95bb7b28750f279
Walkername <05Timur51@gmail.com>
2025-07-27 19:20:06 +0300

    init(file-service): initialization of file service

---
+++ file-service/src/test/java/ru/walkername/file_service/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.walkername.file_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
