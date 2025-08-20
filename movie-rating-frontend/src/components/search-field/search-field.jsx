83440fa0ed6fd645c2523b8beda8f07eef7019b8
Walkername <05Timur51@gmail.com>
2025-08-20 20:02:39 +0300

    feat(frontend): transition for search field and popup; also some design

--- movie-rating-frontend/src/components/search-field/search-field.jsx
+++ movie-rating-frontend/src/components/search-field/search-field.jsx
@@ -1,16 +1,29 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { searchMovieByTitle } from "../../api/movie-api"
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SearchField() {
 
     const [isPopupVisible, setIsPopupVisible] = useState(false);
+    const [isPopupRendered, setIsPopupRendered] = useState(false);
     const [foundMovies, setFoundMovies] = useState([]);
 
     const inputRef = useRef(null);
     const popupRef = useRef(null);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isPopupVisible) {
+            setIsPopupRendered(true);
+        } else {
+            // Ждем завершения анимации перед удалением из DOM
+            const timer = setTimeout(() => {
+                setIsPopupRendered(false);
+            }, 500);
+            return () => clearTimeout(timer);
+        }
+    }, [isPopupVisible]);
 
     const handleNavigate = (target) => {
         // Blur the input field before navigating
@@ -64,18 +77,21 @@ export default function SearchField() {
                 onFocus={() => setIsPopupVisible(true)}
                 onBlur={handleInputBlur}
             />
-            {isPopupVisible && (
-                <div className="popup" ref={popupRef} onMouseDown={handlePopupMouseDown}>
+            {isPopupRendered && (
+                <div
+                    className={`popup ${isPopupVisible ? 'popup-visible' : 'popup-hidden'}`}
+                    ref={popupRef}
+                    onMouseDown={handlePopupMouseDown}>
                     <div className="popup-content">
                         {
                             foundMovies.length > 0 ? (
                                 foundMovies.map((movie, index) => {
                                     let href = "/movies/" + movie.id;
                                     return (
-                                        <div className="movie-div" key={index} onClick={() => handleNavigate(href)}>
+                                        <Link className="movie-div" key={index} to={href}>
                                             {movie.title} ({movie.releaseYear}):
                                             <span style={{ color: "#FF0000" }}> {movie.averageRating}</span>
-                                        </div>
+                                        </Link>
                                     );
                                 })
                             ) : (
