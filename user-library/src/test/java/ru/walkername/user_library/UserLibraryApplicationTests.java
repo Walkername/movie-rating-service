c17881d89b5e96fb543e7efa6fefb76f16da1381
Walkername <05Timur51@gmail.com>
2025-10-18 15:44:22 +0300

    feat(user-library): init project

---
+++ user-library/src/test/java/ru/walkername/user_library/UserLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.walkername.user_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
