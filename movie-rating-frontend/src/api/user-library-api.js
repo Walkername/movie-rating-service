046f63d2059165406e4214ed50a7e1feb414884e
Walkername <05Timur51@gmail.com>
2025-10-22 13:49:57 +0300

    feat(frontend): user-library api to get user rated movies

---
+++ movie-rating-frontend/src/api/user-library-api.js
@@ -0,0 +1,16 @@
+
+export const getMoviesByUser = async (id, page, limit, sort, minRating) => {
+    try {
+        const response = await fetch(
+            `${process.env.REACT_APP_USER_LIBRARY_URL}/user-movies/${id}?page=${page}&limit=${limit}&sort=${sort}`,
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
