1e6253553beb0a69e2a1f806b3304d8e999f8992
Walkername <05Timur51@gmail.com>
2026-02-24 22:55:42 +0300

    fix(movie-catalog): modelMapper to mapStruct; lombok; code refactor

--- movie-catalog/src/main/java/ru/walkername/movie_catalog/MovieCatalogApplication.java
+++ movie-catalog/src/main/java/ru/walkername/movie_catalog/MovieCatalogApplication.java
@@ -1,6 +1,5 @@
 package ru.walkername.movie_catalog;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +10,6 @@ public class MovieCatalogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCatalogApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 
     @Bean
